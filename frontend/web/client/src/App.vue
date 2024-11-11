70ee244b19394298b161aed34eb682360c30dc22
Matei Grosu <70882762+MateiGrosu@users.noreply.github.com>
2024-11-11 10:31:42 +0100

    initializing the web
    - created an easy frontend for the whole system
    - created backend

---
+++ frontend/web/client/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AlarmSettings from './components/AlarmSettings.vue';
+import ApiComponent from './components/ApiComponent.vue';
+import DigitalClock from './components/DigitalClock.vue';
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <HelloWorld msg="Vite + Vue" />
+  <DigitalClock/>
+  <AlarmSettings/>
+  <ApiComponent />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/web/client/src/components/AlarmSettings.vue
@@ -0,0 +1,49 @@
+<script setup lang="js">
+import { ref } from 'vue'
+
+const spraySpeed = ref(0)
+const alarmDelay = ref(0)
+</script>
+
+<template>
+    <div id="app">
+        <h3>Alarm Clock Settings</h3>
+        <label>
+            Water Spray Speed:
+            <input type="range"  v-model="spraySpeed" min="1" max="10" />
+            {{ spraySpeed }}
+        </label>
+        <br>
+        <label>
+            Alarm Delay:
+            <input type="number" class="alarmDelay" v-model="alarmDelay" min="0" /> seconds
+        </label>
+        <br>
+        <button @click="sendSettings">Save Settings</button>
+    </div>
+</template>
+
+<style scoped>
+label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+.alarmDelay{
+    width: 50px;
+}
+
+button {
+    padding: 10px 15px;
+    background-color: #3498db;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #2980b9;
+}
+</style>
