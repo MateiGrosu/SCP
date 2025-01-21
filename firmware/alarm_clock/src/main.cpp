535cfc94253b4addc67651e0fe89c9f33e50fba6
Matei Grosu <70882762+MateiGrosu@users.noreply.github.com>
2025-01-21 12:40:40 +0100

    Update main.cpp

--- firmware/alarm_clock/src/main.cpp
+++ firmware/alarm_clock/src/main.cpp
@@ -9,8 +9,6 @@
 #include <CircularBuffer.hpp>
 #include "display_time.h"
 #include "water_level.h"
-#include "temperature.h"
-#include "lorawan.h"
 
 #include <iostream>
 #include <ArduinoLog.h>
@@ -19,6 +17,7 @@
 using namespace std::chrono;
 
 constexpr unsigned long SERIAL_BAUD = 115200;
+
 constexpr long GMT_OFFSET_SEC = 3600;
 constexpr int DAYLIGHT_OFFSET_SEC = 3600;
 
@@ -29,40 +28,36 @@ Sprinkler sprinkler{};
 AlarmClock timer_handler{ &buzzer, &sprinkler, &button };
 TimeDisplay time_display{};
 WaterLevelSensor water_level_sensor{};
-Temperature temperature{};
-// LoraWAN lorawan{};
 
 void waitForTimeConfigured();
+
 void postSetup();
+
 void slow_loop();
+
 void wait_for_serial();
+
 void connect_to_wifi();
+
 void configure_current_time();
 
 void setup() {
     wait_for_serial();
-    postSetup();
     connect_to_wifi();
     configure_current_time();
+    postSetup();
 }
 
 // When this function is called, time has been synchronized
 void postSetup() {
     Serial.println("Enter post setup");
-
     button.setup();
     buzzer.setup();
     sprinkler.setup();
     time_display.setup();
     water_level_sensor.setup();
 
-    if (!temperature.setup()) { // Initialize Temperature sensor
-        Serial.println("Error: Temperature sensor failed to initialize!");
-    }
-
-    // lorawan.setup(); // Initialize LoRaWAN module
-
-    timer_handler.schedule_alarm(system_clock::now() + chrono::duration<int>(10));
+    timer_handler.schedule_alarm(system_clock::now() += chrono::duration<int>(10));
 
     new SpinTimer(10, new FuncSpinTimerAction([]
     {
@@ -70,7 +65,8 @@ void postSetup() {
     }), true , true);
 }
 
-void slow_loop() {
+void slow_loop()
+{
     button.slow_loop();
 }
 
@@ -79,7 +75,8 @@ void loop() {
     timer_handler.checkAlarm();
 }
 
-void waitForTimeConfigured() {
+void waitForTimeConfigured()
+{
     struct tm timeinfo;
     while (!getLocalTime(&timeinfo)) {
         Serial.println("Waiting for time to be configured...");
@@ -88,26 +85,35 @@ void waitForTimeConfigured() {
     Serial.println("Time is configured!");
 }
 
-void wait_for_serial() {
+
+void wait_for_serial()
+{
     Serial.begin(SERIAL_BAUD);
+    // Wait for the serial port to become available, so we can see what is being printed
     while (!Serial) {}
     Serial.println("Serial port is ready!");
 }
 
-void connect_to_wifi() {
+void connect_to_wifi()
+{
     WiFi.setHostname("Smart Alarm Clock");
     WiFi.setAutoReconnect(true);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     Serial.println("Waiting for WiFi connection...");
+    // The alarm clock needs to current time, which it will receive from the internet.
+    // For that reason, it has to wait until it is connected to the internet.
     while (!WiFi.isConnected()) {}
     Serial.println("Connected to WiFi");
+
+    // Receive the current time from an NTP SERVER
 
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 }
 
-void configure_current_time() {
+void configure_current_time()
+{
     Serial.println("Pulling current time from NTP...");
     configTime(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, "pool.ntp.org");
     waitForTimeConfigured();
